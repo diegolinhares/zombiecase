068c71bdc9be747dbaa5ced9f2fcf544311b0dc1
Diego Linhares <diegomlinhares@gmail.com>
2020-12-25 14:33:05 -0300

    Add JSON-API Mime Type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,31 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :verify_json_api_required_headers
+
   protected
 
   def render_json(status, json = {})
     render status: status, json: json
+  end
+
+  private
+
+  REQUIRED_HEADERS = ['Accept', 'Content-Type'].freeze
+  MIME_TYPE_JSON_API = 'application/vnd.api+json'
+
+  def verify_json_api_required_headers
+    REQUIRED_HEADERS.each do |required_header|
+      header = request.headers[required_header]
+
+      next if header&.include?(MIME_TYPE_JSON_API)
+
+      case required_header
+      when 'Accept'
+        return render json: { error: 'Not Acceptable' }, status: :not_acceptable
+      when 'Content-Type'
+        render json: { error: 'Unsupported Media Type' }, status: :unsupported_media_type
+      end
+    end
   end
 end
--- test/support/request_helper.rb
+++ test/support/request_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 module RequestHelper
+  def json_api_headers
+    {
+      'Accept' => 'application/vnd.api+json',
+      'Content-Type' => 'application/vnd.api+json'
+    }
+  end
+
   def json_response
     JSON.parse(response.body, symbolize_names: true)
   end
