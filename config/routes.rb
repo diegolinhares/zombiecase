8f7e91b577e554ee9f72be911a9a74fbf5e3fc09
Diego Linhares <diegomlinhares@gmail.com>
2020-12-25 01:56:00 -0300

    Update defaults json format to all API

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
+  constraints subdomain: 'api', defaults: { format: :json } do
     namespace :api, path: '/' do
-      resources :survivors, only: %i[create update], defaults: { format: :json } do
+      resources :survivors, only: %i[create update] do
         resources :infection_reports, only: [:create]
         resources :trades, only: [:create]
       end
     
-      resources :reports, only: [:index], defaults: { format: :json }
+      resources :reports, only: [:index]
     
       namespace :reports do
         resource :infected, only: [:show]
